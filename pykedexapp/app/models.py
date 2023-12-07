5698f0b3df66117f092b27cabd184cc6fd45941d
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-12-07 16:41:35 +0100

    django

--- pykedexapp/app/models.py
+++ pykedexapp/app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Pokemon(models.Model):
     name = models.CharField(max_length=30)
     number = models.IntegerField()
     type1 = models.CharField(max_length=30)
     type2 = models.CharField(max_length=30)
+
     def __str__(self):
         return self.name
-
-
