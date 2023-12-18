2626c2b88ac01fe1ce5b53e73a4fc51e23bc2703
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-12-18 19:03:25 +0100

    add: pokemon routes

--- pykedexapp/pokemons/urls.py
+++ pykedexapp/pokemons/urls.py
@@ -1,9 +1,30 @@
 from . import views
 
 urlpatterns = [
-    ('pokeapi/get/<str:name>', views.get_pokemon_from_pokeapi_by_name),
-    ('pokeapi/get/<int:id>', views.get_pokemon_from_pokeapi_by_id),
-    ('pokeapi/get/all', views.get_all_pokemon_from_pokeapi),
+    # [GET] POKEAPI
+    # The following are the endpoints for the PokeAPI
+    # https://pokeapi.co/docs/v2/pokemon
+    ('pokeapi/get/one/<str:name>', views.get_pokemon_from_pokeapi_by_name),
+    ('pokeapi/get/one/<int:id>', views.get_pokemon_from_pokeapi_by_id),
     ('pokeapi/get/some/<int:limit>/<int:offset>', views.get_some_pokemon_from_pokeapi),
-    ('pokeapi/get/
-    ]
+    ('pokeapi/get/some/<str:name>', views.get_some_pokemon_from_pokeapi_by_letters),
+    ('pokeapi/get/all', views.get_all_pokemon_from_pokeapi),
+
+    # [GET] API
+    # The following are the endpoints for the API (pokemons saved in the database)
+    ('get/all', views.get_all_pokemon),
+    ('get/byteamid/<int:team_id>', views.get_pokemon_by_team_id),
+    ('get/byuserid/<int:user_id>/', views.get_pokemon_by_user_id),
+
+    # [POST] API
+    # Pokemons are saved because they are on a team
+    ('post/in_this_team/<int:team_id>', views.post_pokemon),
+
+    # [PUT] API
+    # Pokemons stats might change
+    ('put/<int:id>', views.put_pokemon),
+
+    # [DELETE] API
+    # Pokemons are deleted because they are removed from a team
+    ('delete/<int:id>', views.delete_pokemon),
+]
