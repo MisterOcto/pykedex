5fa2f7c61b09de2e348fe039f5e8f2744b426f21
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-12-18 08:38:42 +0100

    add: models for Pokemon, User and Team

--- pykedexapp/app/models.py
+++ pykedexapp/app/models.py
@@ -3,10 +3,43 @@ from django.db import models
 
 # Create your models here.
 class Pokemon(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    order = models.IntegerField()
     name = models.CharField(max_length=30)
+    lvl = models.IntegerField()
+    xp = models.IntegerField()
     number = models.IntegerField()
-    type1 = models.CharField(max_length=30)
-    type2 = models.CharField(max_length=30)
+    type_1 = models.CharField(max_length=30)
+    type_2 = models.CharField(max_length=30)
+    special_capacity = models.CharField(max_length=30)
+    memo = models.CharField(max_length=30)
+    atck = models.IntegerField()
+    defs = models.IntegerField()
+    atck_spe = models.IntegerField()
+    defs_spe = models.IntegerField()
+    speed = models.IntegerField()
+    hp = models.IntegerField()
+    team_id = models.IntegerField()
 
     def __str__(self):
         return self.name
+
+
+class User(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    user_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
